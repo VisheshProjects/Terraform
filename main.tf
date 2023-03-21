9dee2d44082ce3fb5b62248e17fa18d3248c5761
Vishesh Sharma <vishesh0404@gmail.com>
2023-03-22 01:02:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "verdant-tempest-378609"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "my-instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
